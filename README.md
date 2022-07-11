e4abefc1f93a3150a8337b00aae09e4df3fc735f
badols mito <badols@protonmail.com>
2022-07-11 20:59:09 -0300

    Added license notice when using the program. {fmt} is no longer a dependency, as the program now uses std::cout instead of fmt::print. The user is, under certain conditions, asked whether to proceed or not because accidental overwriting may occur.

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ and change their values. To use your template, simply pass its name as an argume
 ## Dependencies
 
 ProjTmpl uses the following dependencies:
-  - [{fmt}](https://github.com/fmtlib/fmt) for I/O
   - [toml11](https://github.com/ToruNiina/toml11) for parsing and working with TOML files
+  - [Argh!](https://github.com/adishavit/argh) for easy argument handling.
 
 If you do not have {fmt} installed in your system, you will have to install it manually. Note that I'm developing on Ubuntu 22.
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,25 +17,31 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-// TODO: Add notice when interacting with the program as described in
-// https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html#SEC4
-// TODO: Guards preventing the overwriting of files and folders without intention. Use command-line arguments to allow it.
-// TODO: Add shorthands that can be defined in config/main.toml (e. g. a user would be able to use -d instead of default)
-
-#define FMT_HEADER_ONLY 
-#include "fmt/core.h"
-#include "fmt/color.h"
 #include "toml.hpp"
+#include "argh.h"
 
 #include <cstdlib>
 #include <string>
 #include <filesystem>
 #include <fstream>
 #include <vector>
+#include <iostream>
 
 namespace fs = std::filesystem;
 
 int main(int argc, char **argv) {
+
+    std::cout << "This program is licensed under the GPLv2.\n"
+              << "For more information and the terms of this license, visit <https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html>";
+
+    argh::parser cmdl(argv);
+    char inpt;
+    if(!cmdl[{"-ao", "--allow-overwrite"}]) {
+        std::cout << "\n\nBy using this program, the overwriting of certain folders and files may occur.\nDo you still wish to proceed?(y/n)";
+        std::cin >> inpt;
+        if (inpt == 'y') {}
+        else { return 1; } // To guarantee folder and file safety, anything other than 'y' will have the program exit.
+    }
 
     auto data = toml::parse("/usr/local/bin/main.toml");
     std::string template_name;
@@ -54,7 +60,7 @@ int main(int argc, char **argv) {
     std::vector<std::string> directories = toml::get<std::vector<std::string>>(toml::find(tmplt, "directories"));
     for (const auto &i : directories) {
         fs::create_directory(i);
-        fmt::print(fg(fmt::color::light_green), "Directory {} created!\n", i);
+        std::cout << "Directory " << i << " created!\n";
     }
 
     std::vector<std::array<std::string, 2>> files = toml::get<std::vector<std::array<std::string, 2>>>(toml::find(tmplt, "files"));
@@ -62,13 +68,13 @@ int main(int argc, char **argv) {
         std::ofstream j(i[0]); // Creates a file with the first item of i, which is the name for the file
         j << i[1]; // The actual contents get written into that file.
         j.close();
-        fmt::print(fg(fmt::color::light_green), "File {} created!\n", i[0]);
+        std::cout << "File " << i[0] << "created!\n";
     }
 
     std::vector<std::string> commands = toml::get<std::vector<std::string>>(toml::find(tmplt, "commands"));
     for (const auto &i : commands) {
         system(i.data());
-        fmt::print(fg(fmt::color::light_green), "Command {} executed!\n", i);
+        std::cout << "Command " << i <<" executed!\n";
     }
     return 0;
 }
